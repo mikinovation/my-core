762f255f85482dfb20696af8c61527e86f0259be
mikinovation <maneuver2472@gmail.com>
2022-07-02 04:01:21 +0800

    ルーティングを生成

--- philippines/config/routes.rb
+++ philippines/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'components/index'
   root 'components#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'components/index'
+  get "/login", to: "login#index"
 end
